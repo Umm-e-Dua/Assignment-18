e04c9a13e2f521da72b405df833c993fca4215c1
Umm-e-Dua <160483094+Umm-e-Dua@users.noreply.github.com>
2024-06-23 01:37:46 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+// 18 Assignment
+let countriestoVisit = ["Turkey", "Korea", "Brazil", "Chaina"];
+console.log("Original order:",countriestoVisit);
+console.log("Alphabetical Order:", [...countriestoVisit].sort);
+console.log("Still in original order:",countriestoVisit );
+console.log("Reverse Order:",[...countriestoVisit].reverse());
+console.log("Still in original order:",countriestoVisit );
+console.log("Original Array  Reverse:",countriestoVisit.reverse());
+console.log("Back to Original Order: ",countriestoVisit.reverse() );
+console.log(" Sorted in Alphabetical Order:", countriestoVisit.sort());
+console.log("Original Array  Reverse Again:",countriestoVisit.reverse());
